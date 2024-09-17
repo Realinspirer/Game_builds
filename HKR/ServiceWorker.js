68bbffcbbb9b74aabc09a07480e78afd8b319032
Realinspirer <realinspirer@outlook.com>
2024-09-18 00:11:31 +0530

    Added another game build

---
+++ HKR/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Realinspirer-Heck's Kitchen Runner-1.0";
+const contentToCache = [
+    "Build/HKR.loader.js",
+    "Build/HKR.framework.js.unityweb",
+    "Build/HKR.data.unityweb",
+    "Build/HKR.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
