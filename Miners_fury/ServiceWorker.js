a0721eede92c2beef884fdd9ece3febcef8a0cc4
Realinspirer <realinspirer@outlook.com>
2025-07-09 04:53:29 +0530

    Updated build

--- Miners_fury/ServiceWorker.js
+++ Miners_fury/ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "Realinspirer-Miner's Fury-1.0";
 const contentToCache = [
-    "Build/5f9a818a309900368692f886af4e0300.loader.js",
-    "Build/3cf5a741f802c6bb473f9fd8dc9c23ee.framework.js",
-    "Build/8395375b2ac60568f1d9c37389da0351.data",
-    "Build/a345520c4b4041b358c7c187059f736a.wasm",
+    "Build/805c94d496bc3da6798445c587a7444b.loader.js",
+    "Build/17498bd8abe7b4c1a74fcaf9b281748a.framework.js",
+    "Build/95c34f8adae39d92a1bc449b23117d30.data",
+    "Build/adf42b2d1c7247b2117973a7efec7adf.wasm",
     "TemplateData/style.css"
 
 ];
